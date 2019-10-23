57a2861e3ab2407de94abcbb99876b5bd49639aa
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-10-23 23:02:32 +0300

    added Exception Handler MVCadvice

---
+++ src/main/java/uk/co/cetinkaya/msscbeerservice/controller/MvcExceptionHandler.java
@@ -0,0 +1,23 @@
+package uk.co.cetinkaya.msscbeerservice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+	
+	public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex) {
+		List<String> errorsList = new ArrayList<>(ex.getConstraintViolations().size());
+		
+		ex.getConstraintViolations().forEach(error -> errorsList.add(error.toString()));
+		
+		return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+	}
+
+}
